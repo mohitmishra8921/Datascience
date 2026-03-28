0b276e579035bd613e8afbae8f682797c5d823ed
Mohit Mishra <mohitmishra8921@gmail.com>
2026-03-29 00:16:50 +0530

    data visualisation

---
+++ matplot.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt 
+years = [1990, 1992, 1994, 1996, 1998, 2000, 2003, 2005, 2007, 2010]
+kohli = [0, 0, 500, 800, 1100, 1300, 1500, 1800, 1900, 2100]
+sehwag = [0, 300, 800, 1200, 1500, 1700, 1600, 1400, 1000, 0]
+plt.plot(years,kohli,'r^--',label = "Virat Kohli")
+plt.plot(years,sehwag,'bo--',label = "Sehwag")
+plt.xlabel("years")
+plt.ylabel("Runs")
+plt.title("Performance analysis")
+plt.legend()
+plt.show()
